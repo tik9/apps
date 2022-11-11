61307d9551e0889ec05ddf5ce4ea90f682e6a8c1
tik9 <tik9@users.noreply.github.com>
2022-11-11 21:13:33 +0000

    deploy: fc723c06640e0e59622163e2807ff7b5dfc4e252

---
+++ client.js
@@ -0,0 +1,10 @@
+
+document.getElementById("company").textContent = company
+var job_id = document.getElementById("job")
+job_id.textContent = job
+job_id.style.fontStyle = "italic"
+job_id.style.fontWeight = "bold"
+
+document.getElementById("city").textContent = city
+
+if (typeof email != 'undefined') document.getElementById("email").textContent = email
---
+++ env.js
@@ -0,0 +1,7 @@
+
+console.log(2)
+
+var job = 'Wissenschaftlicher Mitarbeiter digitale MINT-Lerneinheiten'
+var company = 'Technische Hochschule Ingolstadt'
+var city = '85049 Ingolstadt'
+var email = ''
